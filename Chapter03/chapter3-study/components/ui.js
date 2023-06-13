597c853c4f5f62f23b06658500cfbbe3f7bb4775
wenrui <wenrui1@muyuanfoods.com>
2023-06-13 15:24:47 +0800

    子组件传递数据到父组件

--- Chapter03/chapter3-study/components/ui.js
+++ Chapter03/chapter3-study/components/ui.js
@@ -1,14 +1,29 @@
 Vue.component('top-bar', {
     template: `<div class="top-bar" :class="'player-'+currentPlayerIndex">
-        <div>{{ players[0].name }}</div>
-        <div>
-            <img src="svg/turn.svg" />
-            <div>Turn {{ turn }}</div>
-        </div>        
-        <div>{{ players[1].name }}</div>
+        <div class="player-0 p0">{{ players[0].name }}</div>
+        <div class="turn-counter">
+            <img class="arrow" src="svg/turn.svg" />
+            <div class="turn">Turn {{ turn }}</div>
+        </div>
+        <div class="player-1">{{ players[1].name }}</div>
     </div>`,
     props: ['players', 'currentPlayerIndex', 'turn'],
     created() {
         console.log(this.players)
+    },
+})
+
+Vue.component('card', {
+    props: ['def'],
+    template: `<div :class="" @click="play" >
+        <div>{{ def.title }}</div>
+        <img src="svg/card-separator.svg" />
+        <div><div v-html="def.description"></div></div>
+        <div v-if="def.note"><div v-html="def.note"></div></div>
+    </div>`,
+    methods: {
+        play () {
+            this.$emit('play-event', 'orange', 42)
+        },
     },
 })
--- Chapter03/chapter3-study/main.js
+++ Chapter03/chapter3-study/main.js
@@ -3,9 +3,23 @@ new Vue({
     el: '#app',
     template: `<div id="#app">
         <top-bar :turn="turn" :current-player-index="currentPlayerIndex" :players="players"/>
+<!--        <card :def="testCard" @click.native="handlePlay"/>-->
+        <card :def="testCard" @play-event="handlePlayEvent"/>
     </div>`,
     data: state,
-
+    computed: {
+        testCard() {
+            return cards.archers
+        },
+    },
+    methods: {
+        handlePlay() {
+            console.log('You played a card!')
+        },
+        handlePlayEvent(color, number) {
+            console.log('handle play event', 'color=', color, 'number=', number)
+        }
+    }
 })
 
 // 窗口大小变化的处理
